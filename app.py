994dd8cf4fd7f0f09317da53200c0c0b1901f33f
Jad Haytaoglu <jadhaytaoglu@hotmail.com>
2023-01-15 00:53:12 -0500

    got playlist searcher and extractor based on determined mood working!

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import json
 import os
 import base64
-from requests import post
+from requests import post, get
+import pprint
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ def get_token():
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    url = "https://account.spotify.com/api/token"
+    url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
@@ -27,13 +28,50 @@ def get_token():
     token = json_result["access_token"]
     return token
 
-token = get_token()
-print(token)
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
 
 
-def get_emotion() -> str:
-    face_analysis = DeepFace.analyze(img_path="images/likeAnimals.jpg")
+def search_for_playlist(tk, mood):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(tk)
+    query = f"?q={mood}&type=playlist&limit=5"
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result['playlists']
+
+
+def get_right_playlist(top5, mood):
+    for pl in top5['items']:
+        if pl['name'] == (mood.capitalize() + " Mix"):
+            return pl
+    return pl
+
+def get_mood(img_path) -> str:
+    face_analysis = DeepFace.analyze(img_path=img_path)
     dominant_emotion = face_analysis['dominant_emotion']
     # print(face_analysis)
-    print(dominant_emotion)
+    # print('dominant emotion: ',dominant_emotion)
     return dominant_emotion
+
+
+def main():
+    token = get_token()
+    img_path = "images/likeAnimals.jpg"
+    mood = get_mood(img_path)
+
+    print('mood: ', mood)
+
+    json_result = search_for_playlist(token, mood)
+    # print('playlist info: \n')
+    # pp = pprint.PrettyPrinter(indent=5)
+    # pp.pprint(json_result)
+
+    playlist = get_right_playlist(json_result, mood)
+    print(playlist['name'])
+
+if __name__ == '__main__':
+    main()
